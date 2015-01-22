1b871062e4be3fcc03b69cfd76d299d4518e83b1
Emīls Gulbis <egu@adevo.io>
2015-01-22 12:33:24 +0200

    Except submit and hidden fields

--- js/jquery.material.form.js
+++ js/jquery.material.form.js
@@ -1,22 +1,29 @@
 $(function(){
 	$.fn.materialForm = function() {
 	    this.find('input, textarea').each(function(){
-	    	var placeholder = $(this).attr('placeholder');
-	    	var $wrap = $(this).wrap("<div class='input'></div>").parent();
+	    	var type = $(this).attr('type');
 
-	    	var tagName = $(this).prop("tagName").toLowerCase();
-	    	$wrap.addClass(tagName);
-	    	
-	    	$wrap.append("<span class='bar'></span>");
-	    	$wrap.append("<label>"+placeholder+"</label>");
-	    	$(this).removeAttr('placeholder');
+	    	if(type != 'hidden' && type != 'submit'){
+		    	var placeholder = $(this).attr('placeholder');
+		    	var $wrap = $(this).wrap("<div class='input'></div>").parent();
+
+		    	var tagName = $(this).prop("tagName").toLowerCase();
+		    	$wrap.addClass(tagName);
+		    	
+		    	$wrap.append("<span class='bar'></span>");
+		    	$wrap.append("<label>"+placeholder+"</label>");
+		    	$(this).removeAttr('placeholder');
+	    	}
 	    });
 
 	    this.find('input, textarea').on('blur', function(){
-	    	if($(this).val())
-	    		$(this).addClass('filled');
-	    	else
-	    		$(this).removeClass('filled');
+	    	var type = $(this).attr('type');
+	    	if(type != 'hidden' && type != 'submit'){
+		    	if($(this).val())
+		    		$(this).addClass('filled');
+		    	else
+		    		$(this).removeClass('filled');
+		    }	
 	    });
 	};
 });
