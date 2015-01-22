831df916685c2a01fe5a21a1159a7c2221e136e6
Emīls Gulbis <egu@adevo.io>
2015-01-22 12:44:42 +0200

    if text already added

--- js/jquery.material.form.js
+++ js/jquery.material.form.js
@@ -1,29 +1,38 @@
 $(function(){
 	$.fn.materialForm = function() {
 	    this.find('input, textarea').each(function(){
-	    	var type = $(this).attr('type');
-
-	    	if(type != 'hidden' && type != 'submit'){
-		    	var placeholder = $(this).attr('placeholder');
+	    	if(isValidType($(this))){
 		    	var $wrap = $(this).wrap("<div class='input'></div>").parent();
+		    	$wrap.append("<span class='bar'></span>");
 
 		    	var tagName = $(this).prop("tagName").toLowerCase();
 		    	$wrap.addClass(tagName);
 		    	
-		    	$wrap.append("<span class='bar'></span>");
-		    	$wrap.append("<label>"+placeholder+"</label>");
-		    	$(this).removeAttr('placeholder');
+		    	var placeholder = $(this).attr('placeholder');
+		    	if(placeholder){
+		    		$wrap.append("<label>"+placeholder+"</label>");
+		    		$(this).removeAttr('placeholder');
+		    	}
+
+		    	addFilled($(this));
 	    	}
 	    });
 
 	    this.find('input, textarea').on('blur', function(){
-	    	var type = $(this).attr('type');
-	    	if(type != 'hidden' && type != 'submit'){
-		    	if($(this).val())
-		    		$(this).addClass('filled');
-		    	else
-		    		$(this).removeClass('filled');
-		    }	
+		    if(isValidType($(this)))
+		    	addFilled($(this))
 	    });
+
+	    function isValidType($el){
+	    	var type = $el.attr('type');
+	    	return (type != 'hidden' && type != 'submit' ? 1 : 0);
+	    }
+
+	    function addFilled($el){
+	    	if($el.val())
+	    		$el.addClass('filled');
+	    	else
+	    		$el.removeClass('filled');
+	    }
 	};
 });
