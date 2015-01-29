91096bd7b127bd5e57a36334c8c152f9cabdb03f
Emīls Gulbis <egu@adevo.io>
2015-01-29 14:27:26 +0200

    Checkbox and radio button added

--- js/jquery.material.form.js
+++ js/jquery.material.form.js
@@ -8,7 +8,7 @@ $(function(){
 
 	$.fn.materialForm = function() {
 		// Inputs
-	    this.find('input, textarea').each(function(){
+	    this.find('input, textarea').each(function(i){
 	    	if(isValidType($(this))){
 	    		var name = $(this).attr('name');
 	    		$(this).attr('id', name);
@@ -26,6 +26,36 @@ $(function(){
 
 		    	addFilled($(this));
 	    	}
+
+	    	if(isType($(this), 'radio') || isType($(this), 'checkbox')){
+	    		
+	    		var name = $(this).attr('name').replace('[]','');
+	    		var group_id = 'material-group-' + name;
+	    		var placeholder = $(this).attr('placeholder');
+	    		var item_id = name+ '-' + i;
+	    		var $label = $('<label for="'+item_id+'">'+placeholder+'</label>');
+	    		var $group_item = $('<div class="material-group-item"></div>');
+	    		$(this).attr('id', item_id);
+
+	    		if($("#"+group_id).length){
+	    			var $group = $('#'+group_id);
+	    			$group.append($(this));
+	    		}
+	    		else{
+	    			var $group = $(this).wrap("<div class='material-group' id='"+group_id+"'></div>").parent();
+	    		}
+
+	    		if(isType($(this), 'radio'))
+	    			var $radio = $('<div class="material-radio"></div>');
+	    		else
+	    			var $radio = $('<div class="material-checkbox"></div>');
+
+	    		$group_item.append($(this));
+	    		$group_item.append($label);
+	    		$group_item.append($radio);
+
+	    		$group.append($group_item);
+	    	}
 	    });
 
 	    this.find('input, textarea').on('blur', function(){
@@ -33,9 +63,16 @@ $(function(){
 		    	addFilled($(this))
 	    });
 
+	    // Radio
+
 	    function isValidType($el){
 	    	var type = $el.attr('type');
 	    	return (type != 'hidden' && type != 'submit' && type != 'checkbox' && type != 'radio' && type != 'file' ? 1 : 0);
+	    }
+
+	    function isType($el, type){
+	    	var el_type = $el.attr('type');
+	    	return (el_type == type);
 	    }
 
 	    function addFilled($el){
